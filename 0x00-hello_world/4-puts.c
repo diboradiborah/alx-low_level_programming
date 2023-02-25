3b26fb770288a2f12f5cfb262ee35910dad55491
diboradiborah <123638382+diboradiborah@users.noreply.github.com>
2023-02-25 15:44:59 +0300

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * main - main block
  *
- * Return: Always 0 (Success)
+ * Return: always(0) (success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
+}
