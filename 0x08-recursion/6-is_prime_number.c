00df648d04004e850a2d5b30d941ef90c731eec6
diboradiborah <diboradiborah@gmail.com>
2023-03-19 07:57:50 -0700

    recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,32 +1,3 @@
-
-Search or jump to…
-Pull requests
-Issues
-Codespaces
-Marketplace
-Explore
- 
-@diboradiborah 
-Dani-1138
-/
-alx-low_level_programming
-Public
-Fork your own copy of Dani-1138/alx-low_level_programming
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-alx-low_level_programming/0x08-recursion/6-is_prime_number.c
-@Dani-1138
-Dani-1138 is prime number
-Latest commit 76e0346 on Jan 4
- History
- 1 contributor
-Executable File  39 lines (33 sloc)  756 Bytes
-
 #include "main.h"
 
 int is_divisible(int num, int div);
