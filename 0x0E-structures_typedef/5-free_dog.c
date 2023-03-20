0dab0a2bed935ef1e7e423470797f636677d4ebb
diboradiborah <diboradiborah@gmail.com>
2023-03-20 13:38:37 -0700

    gytrds

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,17 +1,16 @@
 #include "dog.h"
 #include <stdlib.h>
-
 /**
- * free_dog - Frees dogs.
- * @d: The dog to be freed.
+ * free_dog - frees the dogs..
+ * @d: Pointer to the struct.
+ * Return: None.
  */
-
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		return;
-	free(d->owner);
-	free(d->name);
-	free(d);
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
-Footer
